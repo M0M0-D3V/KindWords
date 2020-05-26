9cd130b175260c48913d5d60f221a27199697375
Monica Hong <M0M0-D3V@users.noreply.github.com>
2020-05-26 16:34:33 -0700

    edit viewrequest

--- client/src/components/ViewRequests.jsx
+++ client/src/components/ViewRequests.jsx
@@ -10,45 +10,11 @@ export default ({ data }) => {
     data.setRequests(data.requests);
   }, [data]);
 
-  const removeFromDom = (requestID) => {
-    data.setRequests(
-      data.requests.filter((request) => request._id !== requestID)
-    );
-  };
+  // const removeFromDom = (requestID) => {
+  //   data.setRequests(
+  //     data.requests.filter((request) => request._id !== requestID)
+  //   );
+  // };
 
-  return (
-    <div>
-      <table className="table table-striped table-bordered table-hover table-condensed">
-        <thead style={{ color: "gray" }}>
-          <th>Team Name</th>
-          <th>Preferred Position</th>
-          <th>Actions</th>
-        </thead>
-        {data.requests.map((request, idx) => {
-          return (
-            <tr>
-              <td key={idx} className="text-left">
-                <Link to={`/requests/${request._id}`}>{request.name}</Link>
-              </td>
-              <td key={idx} className="text-left">
-                {request.preferredPosition}
-              </td>
-              <td key={idx}>
-                <button
-                  className="btn btn-warning btn-sm"
-                  onClick={(e) => navigate(`/requests/edit/${request._id}`)}
-                >
-                  Edit
-                </button>
-                {/* <DeleteButton
-                  requestID={request._id}
-                  successfulCallback={() => removeFromDom(request._id)}
-                ></DeleteButton> */}
-              </td>
-            </tr>
-          );
-        })}
-      </table>
-    </div>
-  );
+  return <div></div>;
 };
