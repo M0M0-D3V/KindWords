e7805506534b9dfa12a9537bd1de7ad2089c5602
LEVI ____ <blanklevi@gmail.com>
2020-05-26 15:46:57 -0700

    turned the buttons at the bottom into modals -Levi

--- client/src/components/Privacy.jsx
+++ client/src/components/Privacy.jsx
@@ -1,15 +1,43 @@
 import React from "react";
+import { Button, Modal } from "react-bootstrap";
+import pikachu from "../img/pikachu.png";
 
-export default () => {
+export default (props) => {
   return (
-    <div className="h-100">
-      <p className="lead">
-        Your privacy is important to us and so is maintaining a safe community.
-        All messages are stored on a secure server and will not be exposd or
-        used for any purposes outside of Kind Words. However, if a message
-        indicates a possibility that a person may do harm to themselves or
-        others, or contains harassment, we may contact relevant authorities.
+    <div>
+      <div className="text-center h-100">
+        <img src={pikachu} alt="pikachu" />
+      </div>
+      <p style={{ color: "#FFC857", textAlign: "center" }}>
+        Kind Words is a place you can come to share about your struggles and
+        pains as well as helping others in their own struggles too. Below are
+        some resources to help guide you.
       </p>
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">Privacy</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p className="lead">
+            Your privacy is important to us and so is maintaining a safe
+            community. All messages are stored on a secure server and will not
+            be exposed or used for any purposes outside of Kind Words. However,
+            if a message indicates a possibility that a person may do harm to
+            themselves or others, or contains harassment, we may contact
+            relevant authorities.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="info" onClick={props.onHide}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
--- client/src/components/WriteRequest.jsx
+++ client/src/components/WriteRequest.jsx
@@ -1,21 +1,52 @@
 import React from "react";
-export default () => {
+import { Button, Modal } from "react-bootstrap";
+import pikachu from "../img/pikachu.png";
+
+export default (props) => {
   return (
-    <div className="h-100">
-      <p className="lead">
-        It can be hard to share your problems! Sometimes the pain is clear and
-        other times you don't really understand why you're upset. Go ahead and
-        let it out. If you want to make it easy for people to reply, consider:
-        <br />
-        - Directly asking if people have experienced something similar.
-        <br />
-        - Talk about your gut feelings
-        <br />- Describe your own behaviors that you don't like.
+    <div>
+      <div className="text-center h-100">
+        <img src={pikachu} alt="pikachu" />
+      </div>
+      <p style={{ color: "#FFC857", textAlign: "center" }}>
+        Kind Words is a place you can come to share about your struggles and
+        pains as well as helping others in their own struggles too. Below are
+        some resources to help guide you.
       </p>
-      <p>
-        Please remember, protect yoruself from strangers online by not sharing
-        your information that could identify you.
-      </p>
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Writing Requests
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            It can be hard to share your problems! Sometimes the pain is clear
+            and other times you don't really understand why you're upset. Go
+            ahead and let it out. If you want to make it easy for people to
+            reply, consider:
+            <br />
+            - Directly asking if people have experienced something similar.
+            <br />
+            - Talk about your gut feelings
+            <br />- Describe your own behaviors that you don't like.
+          </p>
+          <p>
+            Please remember, protect yourself from strangers online by not
+            sharing your information that could identify you.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="info" onClick={props.onHide}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
