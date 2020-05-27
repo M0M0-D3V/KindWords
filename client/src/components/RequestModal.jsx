d2688ac97156dd15f30cb8e1824ae10ae00f141c
Sawyee Beck <sbeck0109@gmail.com>
2020-05-27 12:10:04 -0700

    test

---
+++ client/src/components/RequestModal.jsx
@@ -0,0 +1,42 @@
+// import React, {Component} from 'react';
+// import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
+
+// export class RequestModal extends Component {
+//   constructor(props){
+//     super(props);
+
+//   }
+//   render (){
+//     return (
+//       function MyVerticallyCenteredModal(props) {
+//         return (
+//           <Modal
+//             {...props}
+//             size="lg"
+//             aria-labelledby="contained-modal-title-vcenter"
+//             centered
+//           >
+//             <Modal.Header closeButton>
+//               <Modal.Title id="contained-modal-title-vcenter">
+//                 Create a new request
+//         </Modal.Title>
+//             </Modal.Header>
+//             <Modal.Body>
+//               <div className = "container">
+
+//               </div>
+//               <h4>Centered Modal</h4>
+//               <p>
+//                 Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+//                 dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+//                 consectetur ac, vestibulum at eros.
+//         </p>
+//             </Modal.Body>
+//             <Modal.Footer>
+//               <Button variant = "danger" onClick={props.onHide}>Close</Button>
+//             </Modal.Footer>
+//           </Modal>
+
+//     )
+//   }
+// }
