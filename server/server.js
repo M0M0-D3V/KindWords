1772d2967e282e6f76a3dce4c0a1c30df88c88f8
Sawyee Beck <sbeck0109@gmail.com>
2020-05-27 19:27:40 -0700

    tryinn socket server

--- server/server.js
+++ server/server.js
@@ -16,6 +16,15 @@ require("./routes/user.routes")(app);
 require("./routes/airplane.routes")(app);
 require("./routes/request.routes")(app);
 
-app.listen(process.env.DB_PORT, () =>
+const server = app.listen(process.env.DB_PORT, () =>
   console.log(`Listening on port ${process.env.DB_PORT}`)
 );
+
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("event_from_client", (data) => {
+    socket.broadcast.emit("send_data_to_all_other_clients", data);
+  });
+});
