2fb50e0447ca06aa00eae20f1888a18d8f246405
Sawyee Beck <sbeck0109@gmail.com>
2020-05-27 20:39:23 -0700

    undo

--- server/server.js
+++ server/server.js
@@ -13,18 +13,7 @@ app.use(cors({ credentials: true, origin: "http://localhost:1337" }));
 app.use(express.json());
 
 require("./routes/user.routes")(app);
-require("./routes/airplane.routes")(app);
-require("./routes/request.routes")(app);
 
-const server = app.listen(process.env.DB_PORT, () =>
+app.listen(process.env.DB_PORT, () =>
   console.log(`Listening on port ${process.env.DB_PORT}`)
 );
-
-const io = require("socket.io")(server);
-
-io.on("connection", (socket) => {
-  console.log(socket.id);
-  socket.on("event_from_client", (data) => {
-    socket.broadcast.emit("send_data_to_all_other_clients", data);
-  });
-});
